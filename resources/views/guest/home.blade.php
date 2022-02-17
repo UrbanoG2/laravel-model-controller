5902e3b4e15aad888486548b1742d07d949ad903
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-02-17 17:12:14 +0100

    solved (dd destroyed it all)

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,17 +1,18 @@
 @extends('guest.layouts.base')
 
 @section('content')
+<div class="movie_card">
+
     @foreach ($movies as $movie)
-        <div class="movie_card">
-            <div class="movie_card_contained">
-                <h1> {{ $movie -> title }} </h1>
-                <ul>
-                    <li> {{ $movie -> nationality }} </li>
-                    <li> {{ $movie -> date }} </li>
-                    <li> {{ $movie -> vote }} </li>
-                </ul>
-            </div>
-        </div>  
+        <div class="movie_card_contained">
+            <h1> {{ $movie -> title }} </h1>
+            <ul>
+                <li> Nation: {{ $movie -> nationality }} </li>
+                <li> Release:{{ $movie -> date }} </li>
+                <li> Vote: {{ $movie -> vote }} </li>
+            </ul>
+        </div>
     @endforeach
-            @dd($movie)
+</div>  
+
 @endsection
